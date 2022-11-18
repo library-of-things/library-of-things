89ef5f74e252d0f985c0bd9e4164a9523d229fdb
Robert Shepley <shepleysound@gmail.com>
2022-11-18 14:19:17 -0800

    Restructure components for future use.

---
+++ components/appBar.jsx
@@ -0,0 +1,36 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const navItems = ['Community', 'Profile'];
+
+function DrawerAppBar() {
+  return (
+    <>
+      <AppBar component='nav'>
+        <Toolbar>
+          <Typography
+            variant='h6'
+            component='div'
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Cup of Sugar
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Toolbar />
+    </>
+  );
+}
+
+export default DrawerAppBar;
