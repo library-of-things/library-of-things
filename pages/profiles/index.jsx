0a5bbe0f50f6c633a1bbb8de78de7864ac983859
Robert Shepley <shepleysound@gmail.com>
2022-11-21 15:27:06 -0800

    Change account editing flow.

--- pages/profiles/index.jsx
+++ pages/profiles/index.jsx
@@ -1,8 +1,8 @@
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
 import { Container, Typography } from '@mui/material';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
-import Account from '../components/account';
-import MyProfile from '../components/profile/profile';
+import AccountEdit from '../../components/account-edit';
+import MyProfile from '../../components/profile/profile';
 
 export default function Profile() {
   const session = useSession();
@@ -19,7 +19,7 @@ export default function Profile() {
       ) : (
         <Container>
           <MyProfile />
-          {/* <Account /> */}
+          <AccountEdit />
         </Container>
       )}
     </Container>
