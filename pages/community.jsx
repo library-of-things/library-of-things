d7b46a42d5e35c8738e0341db7e951c78dbde4b7
Tim Traylor <ttraylor83@gmail.com>
2022-11-22 20:24:40 +0000

    working categories buttons and routing to community page

--- pages/community.jsx
+++ pages/community.jsx
@@ -1,28 +1,32 @@
 /* eslint-disable @next/next/no-img-element */
 // import { Container } from '@mui/material';
-import Product from '../components/itemList/itemList';
+import ItemList from '../components/itemList/itemList';
 import cardData from '../components/itemList/cardData';
 import { useState } from 'react';
-import {Grid, styled, Box, Modal } from "@mui/material";
+import { Grid, styled, Box, Modal } from "@mui/material";
 import { AddBoxSharp } from '@mui/icons-material';
 import ShowItemModal from '.././components/modal/modal';
 import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
+import { useRouter } from 'next/router';
 
-export default function Home() {
+export default function CommunityPage() {
+  const router = useRouter();
+
+
   return (
     <>
-			<Product />
-		</>
+      <ItemList initialQuery={router.query} />
+    </>
   );
 }
 
 
 
 
-					{/* {cardData.map(product => {
+{/* {cardData.map(product => {
 						return (
 						<Box
 						padding= "1vw"
